a6ec166debadd64f15066ef416c90aaa2b8c43cd
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 23:28:30 +0100

    bubble sort array for and if loop

--- src/com/spartaglobal/engineering_43/bubble/sort/project/BubbleSorting.java
+++ src/com/spartaglobal/engineering_43/bubble/sort/project/BubbleSorting.java
@@ -1,4 +1,18 @@
 package com.spartaglobal.engineering_43.bubble.sort.project;
 
 public class BubbleSorting {
+
+    public void bubbleSort(int[] numSort){
+
+        int arr = numSort.length;
+        for (int i = 0; i < arr-1; i++)
+            for (int j = 0; j < arr-i-1; j++)
+                if (numSort[j] > numSort[j+1]){
+                    int temp = numSort[j];
+                    numSort[j] = numSort[j+1];
+                    numSort[j+1] = temp;
+                }
+
+        }
+
 }
